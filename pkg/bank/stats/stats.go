89d6c4353d96e2884de0f833ab7897aff84db41a
akmalsulaymonov <akmalsulaymonov@gmail.com>
2022-10-17 17:31:22 +0500

    Autotests

--- pkg/bank/stats/stats.go
+++ pkg/bank/stats/stats.go
@@ -19,3 +19,13 @@ func TotalInCategory(payments []types.Payment, category types.Category) types.Mo
 	}
 	return sum
 }
+
+func FilterByCategory(payments []types.Payment, category types.Category) []types.Payment {
+	var filtered []types.Payment
+	for _, payment := range payments {
+		if payment.Category == category {
+			filtered = append(filtered, payment)
+		}
+	}
+	return filtered
+}
